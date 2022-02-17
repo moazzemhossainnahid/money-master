ba55fe954249db7c366db7ce2dbe5e884ef2d4b1
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-02-17 12:15:06 +0600

    Fourth Commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 
 
-// Handle Income
-
+// get Income Value
 function getIncomeValue(){
     const incomeTxt = document.getElementById('income');
     const income = parseInt(incomeTxt.value);
+
+    // get Income Validation
+    if(incomeTxt.value == ''){
+        document.getElementById('failedI').style.display = 'block';
+    }else if(income < 0 || income == 0){
+        document.getElementById('failedI2').style.display = 'block';
+    }
+    else{
+        document.getElementById('failedI').style.display = 'none';
+    };
+
+
     return income;
 
 };
 
+// get Expense Value
 function getExpensesValue(){
     const foodTxt = document.getElementById('food');
     const food = parseInt(foodTxt.value);
@@ -18,14 +30,42 @@ function getExpensesValue(){
     const others = parseInt(othersTxt.value);
 
     const totalExp = food + rent + others;
+
+    // get Expense Validation
+    if(foodTxt.value == ''){
+        document.getElementById('failedF').style.display = 'block';
+    }else if(rentTxt.value == ''){
+        document.getElementById('failedR').style.display = 'block';
+    }else if(othersTxt.value == ''){
+        document.getElementById('failedO').style.display = 'block';
+    }else if(food < 0 || food == 0){
+        document.getElementById('failedF2').style.display = 'block';
+    }else if(rent < 0 || rent == 0){
+        document.getElementById('failedR2').style.display = 'block';
+    }else if( others < 0 || others == 0){
+        document.getElementById('failedO2').style.display = 'block';
+    }
+    else{
+        document.getElementById('failedF').style.display = 'none';
+        document.getElementById('failedF2').style.display = 'none';
+        document.getElementById('failedR').style.display = 'none';
+        document.getElementById('failedR2').style.display = 'none';
+        document.getElementById('failedO').style.display = 'none';
+        document.getElementById('failedO2').style.display = 'none';
+    };
+
+    
     return totalExp;
 };
 
+// Handle Calculation
 document.getElementById('calculate').addEventListener('click', function(){
     
+    // get Income Expense Function
     const income = getIncomeValue();
     const expense = getExpensesValue();
     const balance = income - expense;
+
 
     // get Total Expenses
     const totalExpenseTxt = document.getElementById('totalExpenses');
@@ -36,7 +76,7 @@ document.getElementById('calculate').addEventListener('click', function(){
     balanceTxt.innerText = balance;
 });
 
-
+// Handle Savings 
 document.getElementById('saveBtn').addEventListener('click', function(){
 
     const income = getIncomeValue();
@@ -49,6 +89,7 @@ document.getElementById('saveBtn').addEventListener('click', function(){
 
     const savings = income * save/100;
 
+    // get Save Amount
     const savingTxt = document.getElementById('savingAmount');
     savingTxt.innerText = savings;
 
